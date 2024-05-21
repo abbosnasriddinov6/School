4c9d6aa957d52f7bf0a78923a7caf1c75af421e8
abbosnasriddinov6 <abbosbeknasriddinov001@gmail.com>
2024-05-22 00:18:15 +0500

    section1

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
 import logo from './images/logo.svg';
 
 
-export {logo };
+export {logo};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,19 @@ import Header from '../../components/header/Header'
 const Home = () => {
   return (
     <div>
-      <Header/>
+      <Header />
+      <div className='Section1'>
+        <div className="container">
+          <div className='background'>
+            <div className='lesson'>
+              <div className="lesson1">
+                <h1>Drasni kichik "yumordan" boshlayman</h1>
+              </div>
+            </div>
+            <button className='button3'>Ariza topshirish</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
