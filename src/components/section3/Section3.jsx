8960262c0e1aec062fcb2c5e68c96dc29d3f90e1
abbosnasriddinov6 <abbosbeknasriddinov001@gmail.com>
2024-05-25 13:41:13 +0500

    section3 done

--- src/components/section3/Section3.jsx
+++ src/components/section3/Section3.jsx
@@ -4,10 +4,10 @@ import { car } from '../../assets'
 const Section3 = () => {
   return (
     <>
-    <div className='Section3'>
-      <div className='container'>
-        <div className="section3">
-          <div className='leftside'>
+      <div className='Section3'>
+        <div className='container'>
+          <div className="section3">
+            <div className='leftside'>
               <h1>Maktab haqida</h1>
               <p>Maktabimiz zamonaviy o'qitish texnikasi, amaliy o'rganish tajribasi va tarbiyaviy muhitni o'z ichiga olgan holda ta'limga innovatsion yondashuvni ta'minlashga qaratilgan. Rahimov School o‘quvchilarga o‘z imkoniyatlarini to‘liq ochib berish va har tomonlama barkamol shaxs bo‘lib yetishishlariga yordam beradi.</p>
               <h2>Chuqurlashtirilgan fanlar:</h2>
@@ -26,13 +26,27 @@ const Section3 = () => {
               <div className='div'>
                 <h4>*5-sinfdan faqat oʻzbek tilida</h4>
               </div>
-          </div>
-          <div className='rightside'>
+            </div>
+            <div className='rightside'>
               <img src={car} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="beautiful">
+            <div className="beaty">
+              <div className="word">
+               <img src="" alt="topolmadim" />
+                <h1>Maktabimizda farzandingizning psixologik, aqliy va jismoniy rivojlanishi uchun barcha shart-sharoitlar mavjud</h1>
+              </div>
+              <div className="backround"></div>
+              <div className="bbutton">
+                <button>Batafsil</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
